18a8a8d9267cde25c25ac86de141144b9807eb90
Nils Saß <nsass@itp.uni-frankfurt.de>
2024-12-27 03:10:29 +0100

    Rearrange spin.h and spin.cpp to make functions accessible for testing

--- src/include/spin.h
+++ src/include/spin.h
@@ -2,7 +2,9 @@
 #define INCLUDE_SPIN_H_
 
 #include <array>
+#include <stdexcept>
 
+#include "const.h"
 #include "gen.h"
 #include "smash/particles.h"
 
@@ -12,6 +14,35 @@ namespace spin {
  * relativistic fluids at global equilibrium" by A. Palermo and F. Becattini
  * (arXiv:2304.02276v2).
  */
+
+// Calculate the square of a four-vector in Minkowski space
+inline double four_vector_square(std::array<double, 4> &four_vector) {
+  double result = 0.0;
+  for (int i = 0; i < 4; i++) {
+    result += metric[i] * four_vector[i] * four_vector[i];
+  }
+  return result;
+}
+
+// Calculate theta from EQ. 42 in arXiv:2304.02276v2 which is needed to
+// calculate the full EQ. 60 afterwards.
+std::array<double, 4> theta(const double mass,
+                            const std::array<double, 16> &vorticity,
+                            const double (&p_lower_index)[4]);
+
+// Calculate the exponent of exp(...) in EQ. (60) from arXiv:2304.02276v2
+inline double exponent(const double k, const double energy_density,
+                       const double temperature, const double mu,
+                       const double theta_squared) {
+  // Check that k is a multiple of 1/2
+  if (std::abs(std::fmod(k, 0.5)) > 1e-6) {
+    throw std::invalid_argument("k must be a multiple of 1/2.");
+  } else if (theta_squared > 0) {
+    throw std::invalid_argument("theta^2 must be negative.");
+  }
+  return (energy_density - mu) / temperature - k * sqrt(-theta_squared);
+}
+
 void calculate_and_set_spin_vector(const gen::element &freezeout_element,
                                    int index_event,
                                    smash::ParticleData ***particle_list,
--- src/spin.cpp
+++ src/spin.cpp
@@ -1,8 +1,6 @@
 #include "spin.h"
 
-#include <array>
 #include <cmath>
-#include <stdexcept>
 #include <vector>
 
 #include "const.h"
@@ -18,10 +16,11 @@ namespace spin {
  * (arXiv:2304.02276v2).
  */
 
-// Calculate theta in EQ. (60) from arXiv:2304.02276v2
-static std::array<double, 4> theta(const double mass,
-                                   const std::array<double, 16> &vorticity,
-                                   const double (&p_lower_index)[4]) {
+// Calculate theta from EQ. 42 in arXiv:2304.02276v2 which is needed to
+// calculate the full EQ. 60 afterwards.
+std::array<double, 4> theta(const double mass,
+                            const std::array<double, 16> &vorticity,
+                            const double (&p_lower_index)[4]) {
   if (mass < small_value) {
     throw std::invalid_argument(
         "Theta cannot be calculated for massless particles.");
@@ -31,41 +30,25 @@ static std::array<double, 4> theta(const double mass,
 
   // Here, we explicitely performed the contractions with the Levi-Civita tensor
   const double theta0 =
-      (-1.0 / mass) * (vorticity[at(2, 3)] * p_lower_index[1] +
-                       vorticity[at(3, 1)] * p_lower_index[2] +
-                       vorticity[at(1, 2)] * p_lower_index[3]);
+      (-1.0 / (2.0 * mass)) * (vorticity[at(2, 3)] * p_lower_index[1] +
+                               vorticity[at(3, 1)] * p_lower_index[2] +
+                               vorticity[at(1, 2)] * p_lower_index[3]);
 
   const double theta1 =
-      (-1.0 / mass) * (vorticity[at(3, 2)] * p_lower_index[0] +
-                       vorticity[at(0, 3)] * p_lower_index[2] +
-                       vorticity[at(2, 0)] * p_lower_index[3]);
+      (-1.0 / (2.0 * mass)) * (vorticity[at(3, 2)] * p_lower_index[0] +
+                               vorticity[at(0, 3)] * p_lower_index[2] +
+                               vorticity[at(2, 0)] * p_lower_index[3]);
 
   const double theta2 =
-      (-1.0 / mass) * (vorticity[at(1, 3)] * p_lower_index[0] +
-                       vorticity[at(3, 0)] * p_lower_index[1] +
-                       vorticity[at(0, 1)] * p_lower_index[3]);
+      (-1.0 / (2.0 * mass)) * (vorticity[at(1, 3)] * p_lower_index[0] +
+                               vorticity[at(3, 0)] * p_lower_index[1] +
+                               vorticity[at(0, 1)] * p_lower_index[3]);
 
   const double theta3 =
-      (-1.0 / mass) * (vorticity[at(2, 1)] * p_lower_index[0] +
-                       vorticity[at(0, 2)] * p_lower_index[1] +
-                       vorticity[at(1, 0)] * p_lower_index[2]);
+      (-1.0 / (2.0 * mass)) * (vorticity[at(2, 1)] * p_lower_index[0] +
+                               vorticity[at(0, 2)] * p_lower_index[1] +
+                               vorticity[at(1, 0)] * p_lower_index[2]);
   return {theta0, theta1, theta2, theta3};
-}
-
-// Calculate the square of a four-vector in Minkowski space
-static double four_vector_square(std::array<double, 4> &four_vector) {
-  double result = 0.0;
-  for (int i = 0; i < 4; i++) {
-    result += metric[i] * four_vector[i] * four_vector[i];
-  }
-  return result;
-}
-
-// Calculate the exponent of exp(...) in EQ. (60) from arXiv:2304.02276v2
-static double exponent(const int k, const double energy_density,
-                       const double temperature, const double mu,
-                       const double theta_squared) {
-  return (energy_density - mu) / temperature - k * sqrt(-theta_squared);
 }
 
 void calculate_and_set_spin_vector(const gen::element &freezeout_element,
@@ -112,7 +95,7 @@ void calculate_and_set_spin_vector(const gen::element &freezeout_element,
     int array_index = 0;
     // Calculate all terms of the sums in the numerator and denominator
     // separately and store them in arrays
-    for (int k = -spin; k <= spin; k++) {
+    for (int k = -spin; k <= spin; k += 2) {
       // Ensure that the loop index is within the array bounds
       if (array_index < 0 || array_index >= spin_degeneracy) {
         throw std::out_of_range("Array index is out of range.");
@@ -121,7 +104,7 @@ void calculate_and_set_spin_vector(const gen::element &freezeout_element,
           std::exp(exponent(k, energy_density, temperature, mu, theta_squared));
       denominator_vector.push_back(1 /
                                    (exponential - (spin % 2 == 0 ? 1 : -1)));
-      numerator_vector.push_back(k * denominator_vector[array_index]);
+      numerator_vector.push_back(k * 0.5 * denominator_vector[array_index]);
       array_index++;
     }
     // Performing the sum over k in the numerator and denominator
