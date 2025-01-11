4f71abe97065a8b0fcaa093a95e9521dc9474b05
Robin Sattler <sattler@itp.uni-frankfurt.de>
2025-01-11 17:29:42 +0100

    Remove obsolete variables
    Removed variables `NBINS`, `QMAX`, `rescatter`, and `weakContribution`
    which were initialized but never used in the code.

--- src/include/params.h
+++ src/include/params.h
@@ -1,22 +1,17 @@
 #ifndef INCLUDE_PARAMS_H_
 #define INCLUDE_PARAMS_H_
 
-namespace params{
+namespace params {
 extern char sSurface [255], sSpectraDir [255];
-extern bool weakContribution ;
-extern bool rescatter ;
-extern bool shear ;
-extern bool bulk ;
-//extern double Temp, mu_b, mu_q, mu_s ;
-extern int NEVENTS ;
-extern double NBINS, QMAX ;
-extern double dx, dy, deta ;
-extern double ecrit, cs2, ratio_pressure_energydensity ;
-extern bool createRootOutput;
+extern bool bulk, createRootOutput, shear;
+extern int NEVENTS;
+extern double dx, dy, deta;
+extern double ecrit, cs2, ratio_pressure_energydensity;
+//extern double Temp, mu_b, mu_q, mu_s;
 
-// ---- rooutines ----
-void readParams(char* filename) ;
-void printParameters() ;
+// ---- Routines ----
+void readParams(char* filename);
+void printParameters();
 }
 
 #endif // INCLUDE_PARAMS_H_
--- src/params.cpp
+++ src/params.cpp
@@ -8,21 +8,14 @@
 
 using namespace std ;
 
-namespace params{
+namespace params {
 
 char sSurface [255], sSpectraDir [255];
-bool weakContribution ;
-bool rescatter ;
-bool shear ;
-bool bulk ;
+bool bulk{false}, createRootOutput{false}, shear{false};
+int NEVENTS;
+double dx{0}, dy{0}, deta{0.05};
+double ecrit, cs2{0.15}, ratio_pressure_energydensity{0.15};
 //double Temp, mu_b, mu_q, mu_s ;
-int NEVENTS ;
-double NBINS, QMAX ;
-double dx, dy, deta ;
-double ecrit ;
-double cs2=0.15;
-double ratio_pressure_energydensity=0.15;
-bool createRootOutput {false};
 
 // ############ reading and processing the parameters
 
@@ -38,11 +31,7 @@ void readParams(char* filename)
 		sline >> parName >> parValue ;
 		if     (strcmp(parName,"surface")==0) strcpy(sSurface, parValue) ;
 		else if(strcmp(parName,"spectra_dir")==0) strcpy(sSpectraDir, parValue) ;
-		else if(strcmp(parName,"Nbins")==0) NBINS = atoi(parValue) ;
-		else if(strcmp(parName,"q_max")==0) QMAX = atof(parValue) ;
 		else if(strcmp(parName,"number_of_events")==0) NEVENTS = atoi(parValue) ;
-		else if(strcmp(parName,"rescatter")==0) rescatter = atoi(parValue) ;
-		else if(strcmp(parName,"weakContribution")==0) weakContribution = atoi(parValue) ;
 		else if(strcmp(parName,"shear")==0) shear = atoi(parValue) ;
 		else if(strcmp(parName,"bulk")==0) bulk = atoi(parValue) ;
 		else if(strcmp(parName,"ecrit")==0) ecrit = atof(parValue) ;
@@ -52,22 +41,17 @@ void readParams(char* filename)
 		else if(parName[0]=='!') cout << "CCC " << sline.str() << endl ;
 		else cout << "UUU " << sline.str() << endl ;
 	}
- deta=0.05 ; dx=dy=0.0 ; // TODO!
 }
 
 void printParameters()
 {
-  cout << "====== parameters ======\n" ;
+  cout << "======= parameters ===========\n" ;
   cout << "surface = " << sSurface << endl ;
   cout << "spectraDir = " << sSpectraDir << endl ;
   cout << "numberOfEvents = " << NEVENTS << endl ;
-  cout << "isRescatter = " << rescatter << endl ;
-  cout << "weakContribution = " << weakContribution << endl ;
   cout << "shear_visc_on = " << shear << endl ;
   cout << "bulk_visc_on = " << bulk << endl ;
   cout << "e_critical = " << ecrit << endl ;
-  cout << "Nbins = " << NBINS << endl ;
-  cout << "q_max = " << QMAX << endl ;
   cout << "cs2 = " << cs2 << endl ;
   cout << "ratio_pressure_energydensity = " << ratio_pressure_energydensity << endl ;
   cout << "createRootOutput = " << createRootOutput << endl ;
